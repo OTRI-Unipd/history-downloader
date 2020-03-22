1f7674dbb99a45fedc4f289155b99b47e3a2b6b0
Luca Crema <lc.crema@hotmail.com>
2020-03-23 00:23:53 +0100

    feat(yf_downloader): added download by period of time + download_period(ticker, period, interval) + __output_JSON_filename_period(ticker, period) ~ __output_JSON_filename -> __output_JSON_filename_dates + exported output JSON file writing inside __write_on_JSON_file ~ __output_JSON_filename_dates fixed incorrect date format

--- yf_downloader.py
+++ yf_downloader.py
@@ -3,6 +3,7 @@ Classes:
 YFinanceDownloader -- Download all kind of data from Yahoo Finance
 '''
 import yfinance as yf
+from pandas import  DataFrame
 from pathlib import Path
 from datetime import datetime, timedelta, date
 
@@ -23,6 +24,26 @@ class YFinanceDownloader:
         '''
         self.files_directory = files_directory
     
+    def download_period(self, ticker : str, period : str, interval : str = "1d"):
+        '''
+        Downloads quote data for a single ticker given a period of time from today.
+
+        Parameters:
+            ticker : str
+                The simbol to download data of.
+            period : str
+                Period of time that could be 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
+            interval : str
+                Could be "1m" (7 days max); "2m", "5m", "15m", "30m", "90m" (60 days max); "60m", "1h" (730 days max); "1d", "5d", "1wk"
+        '''
+        yf_data = yf.download(ticker, period = period, interval = interval, round  = False, progress = False)
+
+        # If no data is downloaded it means that the ticker couldn't be found or there has been an error, we're not creating any output file then.
+        if yf_data.empty:
+            return False
+
+        return self.__write_on_JSON_file(self.__output_JSON_filename_period(ticker, period), yf_data)
+
     def download_dates(self, ticker : str, start_date : date, end_date : date, interval : str = "1d"):
         '''
         Downloads quote data for a single ticker given the start date and end date.
@@ -37,16 +58,14 @@ class YFinanceDownloader:
             interval : str
                 Could be "1m" (7 days max); "2m", "5m", "15m", "30m", "90m" (60 days max); "60m", "1h" (730 days max); "1d", "5d", "1wk"
         '''
-        json_filepath = Path(self.files_directory, self.__output_JSON_filename(ticker, start_date, end_date))
-
+       
         yf_data = yf.download(ticker, start = self.__yahoo_time_format(start_date), end = self.__yahoo_time_format(end_date), interval = interval, round  = False, progress = False)
 
         # If no data is downloaded it means that the ticker couldn't be found or there has been an error, we're not creating any output file then.
         if yf_data.empty:
             return False
 
-        json_filepath.open("w+").write(yf_data.to_json(orient="table", indent = 4))
-        return json_filepath
+        return self.__write_on_JSON_file(self.__output_JSON_filename_dates(ticker, start_date, end_date), yf_data)
 
     def download_minimum_interval(self, ticker : str, start_date : date, end_date : date):
         '''
@@ -77,6 +96,19 @@ class YFinanceDownloader:
 
         return self.download_minimum_interval(ticker, seven_days_ago, today)
 
+    def __write_on_JSON_file(self, json_filename : str, yf_data : DataFrame):
+        '''
+        Creates a new file in the files_directory and fills it with tabled Dataframe
+
+        Parameters:
+            json_filename : str
+            yf_data : DataFrame
+                Downloaded data with yf.download()
+        '''
+        json_filepath = Path(self.files_directory, json_filename)
+        json_filepath.open("w+").write(yf_data.to_json(orient="table", indent = 4))
+        return json_filepath
+
     def __yahoo_time_format(self, date : date):
         '''
         Formats time into yfinance-ready string format for start date and end date.
@@ -87,7 +119,7 @@ class YFinanceDownloader:
         '''
         return date.strftime("%Y-%m-%d")
 
-    def __output_JSON_filename(self, ticker : str, start_date : date, end_date : date):
+    def __output_JSON_filename_dates(self, ticker : str, start_date : date, end_date : date):
         '''
         Generates the JSON output file's name using ticker, data start date and end date.
 
@@ -99,7 +131,19 @@ class YFinanceDownloader:
             end_datetime : datetime
                 End date of output data.
         '''
-        return "{}_{}-to-{}.json".format(ticker, end_date.strftime("%d-%m-%y"), start_date.strftime("$d-%m-%y"))
+        return "{}_{}_to_{}.json".format(ticker, end_date.strftime("%d-%m-%y"), start_date.strftime("%d-%m-%y"))
+
+    def __output_JSON_filename_period(self, ticker : str, period : str):
+        '''
+        Generates the JSON output file's name using ticker and period of time.
+
+        Parameters:
+            ticker : str
+                Output ticker's name.
+            period : str
+                Yahoo's period of time
+        '''
+        return "{}_{}_{}.json".format(ticker, date.today().strftime("%d-%m-%y"), period)
 
     def __calc_minimum_interval(self, start_date : date):
         '''
