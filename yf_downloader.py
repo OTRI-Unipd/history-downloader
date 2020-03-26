71872d00adf712bef5268fa9f9376af65297c02c
Luca Crema <lc.crema@hotmail.com>
2020-03-27 00:32:31 +0100

    fix(YF): moved functions to static methods

--- yf_downloader.py
+++ yf_downloader.py
@@ -7,66 +7,6 @@ from datetime import date, timedelta
 from pandas import  DataFrame
 import yfinance as yf
 
-# Functions
-
-def __yahoo_time_format(date : date):
-    '''
-    Formats time into yfinance-ready string format for start date and end date.
-
-    Parameters:
-        date : datetime.date
-            Date to be formatted for yfinance start and end times.
-    '''
-    return date.strftime("%Y-%m-%d")
-
-def __output_JSON_filename_dates(ticker : str, start_date : date, end_date : date):
-    '''
-    Generates the JSON output file's name using ticker, data start date and end date.
-
-    Parameters:
-        ticker : str
-            Output ticker's name.
-        start_datetime : datetime
-            Beginning date of output data.
-        end_datetime : datetime
-            End date of output data.
-    '''
-    return "{}_{}_to_{}.json".format(ticker, end_date.strftime("%d-%m-%y"), start_date.strftime("%d-%m-%y"))
-
-def __output_JSON_filename_period(ticker : str, period : str):
-    '''
-    Generates the JSON output file's name using ticker and period of time.
-
-    Parameters:
-        ticker : str
-            Output ticker's name.
-        period : str
-            Yahoo's period of time
-    '''
-    return "{}_{}_{}.json".format(ticker, date.today().strftime("%d-%m-%y"), period)
-
-def __calc_minimum_interval(start_date : date):
-    '''
-    Calculates the shortest interval (from 1min to 1day) allowed for the given interval. For the calculation we only need the start datetime because
-    it depends on how old is the wanted data.
-
-    Parameters:
-        start_datetime: datetime
-            Beginning date of wanted data.
-
-    Returns: The correct shortest yfinance interval
-    '''
-    days_difference = (date.today() - start_date).days
-
-    if days_difference <= 7:
-        return "1m"
-    elif days_difference <= 60:
-        return "2m"
-    elif days_difference <= 730:
-        return "1h"
-    else:
-        return "1d"
-
 class YFinanceDownloader:
     '''
     Attributes:
@@ -101,7 +41,7 @@ class YFinanceDownloader:
         if yf_data.empty:
             return False
 
-        return self.__write_on_JSON_file(__output_JSON_filename_period(ticker, period), yf_data)
+        return self.__write_on_JSON_file(YFinanceDownloader.__output_JSON_filename_period(ticker, period), yf_data)
 
     def download_dates(self, ticker : str, start_date : date, end_date : date, interval : str = "1d"):
         '''
@@ -118,13 +58,13 @@ class YFinanceDownloader:
                 Could be "1m" (7 days max); "2m", "5m", "15m", "30m", "90m" (60 days max); "60m", "1h" (730 days max); "1d", "5d", "1wk"
         '''
 
-        yf_data = yf.download(ticker, start=__yahoo_time_format(start_date), end=__yahoo_time_format(end_date), interval=interval, round=False, progress=False)
+        yf_data = yf.download(ticker, start=YFinanceDownloader.__yahoo_time_format(start_date), end=YFinanceDownloader.__yahoo_time_format(end_date), interval=interval, round=False, progress=False)
 
         # If no data is downloaded it means that the ticker couldn't be found or there has been an error, we're not creating any output file then.
         if yf_data.empty:
             return False
 
-        return self.__write_on_JSON_file(__output_JSON_filename_dates(ticker, start_date, end_date), yf_data)
+        return self.__write_on_JSON_file(YFinanceDownloader.__output_JSON_filename_dates(ticker, start_date, end_date), yf_data)
 
     def download_minimum_interval(self, ticker : str, start_date : date, end_date : date):
         '''
@@ -139,7 +79,7 @@ class YFinanceDownloader:
             end_datetime : datetime
                 Must be after start_datetime.
         '''
-        return self.download_dates(ticker, start_date, end_date, __calc_minimum_interval(start_date))
+        return self.download_dates(ticker, start_date, end_date, YFinanceDownloader.__calc_minimum_interval(start_date))
 
 
     def download_last_week(self, ticker : str):
@@ -167,3 +107,65 @@ class YFinanceDownloader:
         json_filepath = Path(self.files_directory, json_filename)
         json_filepath.open("w+").write(yf_data.to_json(orient="table", indent=4))
         return json_filepath
+
+    @staticmethod
+    def __yahoo_time_format(date : date):
+        '''
+        Formats time into yfinance-ready string format for start date and end date.
+
+        Parameters:
+            date : datetime.date
+                Date to be formatted for yfinance start and end times.
+        '''
+        return date.strftime("%Y-%m-%d")
+
+    @staticmethod
+    def __output_JSON_filename_dates(ticker : str, start_date : date, end_date : date):
+        '''
+        Generates the JSON output file's name using ticker, data start date and end date.
+
+        Parameters:
+            ticker : str
+                Output ticker's name.
+            start_datetime : datetime
+                Beginning date of output data.
+            end_datetime : datetime
+                End date of output data.
+        '''
+        return "{}_{}_to_{}.json".format(ticker, end_date.strftime("%d-%m-%y"), start_date.strftime("%d-%m-%y"))
+
+    @staticmethod
+    def __output_JSON_filename_period(ticker : str, period : str):
+        '''
+        Generates the JSON output file's name using ticker and period of time.
+
+        Parameters:
+            ticker : str
+                Output ticker's name.
+            period : str
+                Yahoo's period of time
+        '''
+        return "{}_{}_{}.json".format(ticker, date.today().strftime("%d-%m-%y"), period)
+
+    @staticmethod
+    def __calc_minimum_interval(start_date : date):
+        '''
+        Calculates the shortest interval (from 1min to 1day) allowed for the given interval. For the calculation we only need the start datetime because
+        it depends on how old is the wanted data.
+
+        Parameters:
+            start_datetime: datetime
+                Beginning date of wanted data.
+
+        Returns: The correct shortest yfinance interval
+        '''
+        days_difference = (date.today() - start_date).days
+
+        if days_difference <= 7:
+            return "1m"
+        elif days_difference <= 60:
+            return "2m"
+        elif days_difference <= 730:
+            return "1h"
+        else:
+            return "1d"
