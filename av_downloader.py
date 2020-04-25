763be13b69cad15ad34eb4755a02dc091d7e76e7
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-25 11:24:27 +0200

    feat (av_downloader.py): Now converts all datetime keys to GMT + added static method __convert_to_gmt(datetime, timezone : string)

--- av_downloader.py
+++ av_downloader.py
@@ -7,15 +7,17 @@ If main module then requests a key and symbol via user input and stores in a sub
 '''
 from alpha_vantage.timeseries import TimeSeries
 from datetime import datetime
+from pytz import timezone
 from pathlib import Path
 import json
+import pytz
 
+GMT = timezone("GMT")
 TIME_ZONE_KEY = "6. Time Zone"
 
+
 class AVDownloader:
-    '''
-    '''
-    def __init__(self, key : str, dir_path : Path):
+    def __init__(self, key: str, dir_path: Path):
         '''
         Init method.
         Parameters:
@@ -26,8 +28,8 @@ class AVDownloader:
         '''
         self.ts = TimeSeries(key)
         self.dir_path = dir_path
-    
-    def download(self, symbol : str, interval='1min'):
+
+    def download(self, symbol: str, interval='1min'):
         '''
         Downloads data for a single symbol.
         Downloads the full record of the TimeSeries Alpha Vantage api and stores the result in a file, returning the output file.
@@ -43,23 +45,51 @@ class AVDownloader:
 
         time_now = datetime.now()
         string_date = time_now.strftime('%H_%M_%S_%d_%m_%Y')
-        file_name = '{}_{}_{}.json'.format(symbol, interval, string_date);
+        file_name = '{}_{}_{}.json'.format(symbol, interval, string_date)
         file_path = Path(self.dir_path, file_name)
-        
+
         try:
-            data, meta = self.ts.get_intraday(symbol, interval=interval, outputsize='full')
+            data, meta = self.ts.get_intraday(
+                symbol, interval=interval, outputsize='full')
             # The meta retrieved here are not what we want
-            timezone = meta[TIME_ZONE_KEY]
+
+            tz = meta[TIME_ZONE_KEY]
+            new_data = dict()
+            for k, v in data.items():
+                # Datetime key and atom value
+                new_data[
+                    AVDownloader.__convert_to_gmt(
+                        date_time=datetime.strptime(k, "%Y-%m-%d %H:%M:%S"),
+                        zonename=tz
+                    ).strftime("%Y-%m-%d %H:%M:%S")
+                ] = v
             meta = dict()
             meta['ticker'] = symbol
             meta['interval'] = interval
             meta['provider'] = "alpha vantage"
-            data['metadata'] = meta
+            new_data['metadata'] = meta
             with file_path.open('w+') as result_file:
-                json.dump(data, result_file, indent=4)
+                json.dump(new_data, result_file, indent=4)
             return file_path
         except ValueError:
-                return None
+            return None
+
+    @staticmethod
+    def __convert_to_gmt(*, date_time: datetime, zonename: str) -> datetime:
+        '''
+        Method to convert a datetime in a certain timezone to a GMT datetime
+        Parameters:
+            date_time : datetime:
+                The datetime to convert
+            zonename : str
+                The time zone's name
+        Returns:
+            The datetime object in GMT time
+        '''
+        zone = timezone(zonename)
+        base = zone.localize(date_time)
+        return base.astimezone(GMT)
+
 
 if __name__ == '__main__':
     dir_path = Path('/av_historical_1m')
